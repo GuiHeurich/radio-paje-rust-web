c5937062fdde5e77c1004cf8bcd460e7c8fd58cd
Gui Heurich <guilherme.heurich@protonmail.com>
2025-09-27 14:02:15 +0100

    Replace bespoke server with actix_web

--- src/main.rs
+++ src/main.rs
@@ -1,118 +1,47 @@
-use radio_paje_rust_web::ThreadPool;
 use std::{
-    env,
-    fs,
-    io::{BufReader, prelude::*},
-    net::{TcpListener, TcpStream}
+    collections::HashMap,
 };
+use actix_web::{web, App, HttpServer, Responder};
+use askama::Template;
 
-fn main() {
-    let port = env::var("PORT").unwrap_or_else(|_| "7878".to_string());
-    let addr = format!("127.0.0.1:{}", port);
+#[derive(Template)]
+#[template(path = "index.html")]
+struct Index;
 
-    let listener = TcpListener::bind(&addr).unwrap();
+async fn index(_query: web::Query<HashMap<String, String>>) -> Result<impl Responder, actix_web::Error> {
+    let html = Index.render().expect("template should be valid");
 
-    println!("Server listening on port {}", port);
-    println!("http://{}", addr);
-
-    let pool = ThreadPool::new(20);
-
-    for stream in listener.incoming() {
-        let stream = stream.unwrap();
-
-        pool.execute(|| {
-            handle_connection(stream);
-        });
-    }
+    Ok(web::Html::new(html))
 }
 
-fn handle_connection(mut stream: TcpStream) {
-    let buf_reader = BufReader::new(&stream);
-    let request_line = match buf_reader.lines().next() {
-        Some(Ok(line)) => line,
-        _ => {
-            // Add logging here, in the future;
-            return;
-        }
-    };
-
-    let (status_line, filename) = match &request_line[..] {
-        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "home.html"),
-        "GET /hello HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
-        _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
-    };
-
-    let contents = fs::read_to_string(filename).unwrap();
-    let length = contents.len();
-
-    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
-
-    stream.write_all(response.as_bytes()).unwrap();
+#[actix_web::main] // or #[tokio::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(web::resource("/").route(web::get().to(index)))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use std::fs;
-    use std::io::{Read, Write};
-    use std::net::{TcpListener, TcpStream};
-    use std::thread;
+    use actix_web::{test, web, App};
 
-    #[test]
-    fn handle_connection_renders_home_html() {
-        let expected = fs::read_to_string("test_data/home_test.html").unwrap();
+    #[actix_web::test]
+    async fn test_index_returns_html() {
+        let mut app = test::init_service(
+            App::new().service(web::resource("/").route(web::get().to(index)))
+        ).await;
 
-        let listener = TcpListener::bind("127.0.0.1:0").unwrap();
-        let addr = listener.local_addr().unwrap();
-        thread::spawn(move || {
-            let (stream, _) = listener.accept().unwrap();
-            handle_connection(stream);
-        });
-        let mut stream = TcpStream::connect(addr).unwrap();
-        stream.write_all(b"GET / HTTP/1.1\r\n\r\n").unwrap();
-        let mut buffer = String::new();
-        stream.read_to_string(&mut buffer).unwrap();
+        let req = test::TestRequest::get().uri("/").to_request();
+        let resp = test::call_service(&mut app, req).await;
+        assert!(resp.status().is_success());
+        let body = test::read_body(resp).await;
 
-        assert!(buffer.contains("HTTP/1.1 200 OK"));
-        assert!(buffer.contains(&expected));
-    }
-
-    #[test]
-    fn handle_connection_renders_hello_html() {
-        let expected = fs::read_to_string("test_data/hello_test.html").unwrap();
-
-        let listener = TcpListener::bind("127.0.0.1:0").unwrap();
-        let addr = listener.local_addr().unwrap();
-        thread::spawn(move || {
-            let (stream, _) = listener.accept().unwrap();
-            handle_connection(stream);
-        });
-        let mut stream = TcpStream::connect(addr).unwrap();
-        stream.write_all(b"GET /hello HTTP/1.1\r\n\r\n").unwrap();
-        let mut buffer = String::new();
-        stream.read_to_string(&mut buffer).unwrap();
-
-        assert!(buffer.contains("HTTP/1.1 200 OK"));
-        assert!(buffer.contains(&expected));
-    }
-
-    #[test]
-    fn handle_connection_returns_404_for_other_paths() {
-        let expected = fs::read_to_string("test_data/hello_404_test.html").unwrap();
-
-        let listener = TcpListener::bind("127.0.0.1:0").unwrap();
-        let addr = listener.local_addr().unwrap();
-        thread::spawn(move || {
-            let (stream, _) = listener.accept().unwrap();
-            handle_connection(stream);
-        });
-        let mut stream = TcpStream::connect(addr).unwrap();
-        stream
-            .write_all(b"GET /doesnotexist HTTP/1.1\r\n\r\n")
-            .unwrap();
-        let mut buffer = String::new();
-        stream.read_to_string(&mut buffer).unwrap();
-        assert!(buffer.contains("HTTP/1.1 404 NOT FOUND"));
-        assert!(buffer.contains(&expected));
+        let body_str = String::from_utf8_lossy(&body);
+        assert!(body_str.contains("<html") || body_str.contains("<!DOCTYPE html"));
     }
 }
