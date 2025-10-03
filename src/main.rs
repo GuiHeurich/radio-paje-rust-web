bf19efd159986611737a09d6060c7bf5cce707e5
Gui Heurich <guilherme.heurich@protonmail.com>
2025-10-03 15:20:38 +0100

    Add basic websocket functionality

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,29 @@
-use std::{
-    collections::HashMap,
-};
-use actix_web::{web, App, HttpServer, Responder};
+use actix_web::{App, Error, HttpRequest, HttpResponse, HttpServer, Responder, rt, web};
 use askama::Template;
+use std::collections::HashMap;
+
+mod handler;
 
 #[derive(Template)]
 #[template(path = "index.html")]
 struct Index;
 
-async fn index(_query: web::Query<HashMap<String, String>>) -> Result<impl Responder, actix_web::Error> {
+async fn index(
+    _query: web::Query<HashMap<String, String>>,
+) -> Result<impl Responder, actix_web::Error> {
     let html = Index.render().expect("template should be valid");
 
     Ok(web::Html::new(html))
+}
+
+/// Handshake and start WebSocket handler with heartbeats.
+async fn echo_heartbeat_ws(req: HttpRequest, stream: web::Payload) -> Result<HttpResponse, Error> {
+    let (res, session, msg_stream) = actix_ws::handle(&req, stream)?;
+
+    // spawn websocket handler (and don't await it) so that the response is returned immediately
+    rt::spawn(handler::echo_heartbeat_ws(session, msg_stream));
+
+    Ok(res)
 }
 
 #[actix_web::main] // or #[tokio::main]
@@ -19,6 +31,7 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .service(web::resource("/").route(web::get().to(index)))
+            .service(web::resource("/echo").route(web::get().to(echo_heartbeat_ws)))
     })
     .bind(("127.0.0.1", 8080))?
     .run()
@@ -28,13 +41,13 @@ async fn main() -> std::io::Result<()> {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use actix_web::{test, web, App};
+    use actix_web::{App, test, web};
 
     #[actix_web::test]
     async fn test_index_returns_html() {
-        let mut app = test::init_service(
-            App::new().service(web::resource("/").route(web::get().to(index)))
-        ).await;
+        let mut app =
+            test::init_service(App::new().service(web::resource("/").route(web::get().to(index))))
+                .await;
 
         let req = test::TestRequest::get().uri("/").to_request();
         let resp = test::call_service(&mut app, req).await;
